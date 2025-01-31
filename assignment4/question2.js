1c1a18c0e4c9ed10f5e73b5bf13c87fd8566f646
cerdicwongg <cerdicwong@hotmail.com>
2025-01-31 19:55:29 +0800

    up to question2
    returns NaN

--- assignment4/question2.js
+++ assignment4/question2.js
@@ -1,3 +1,20 @@
 const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82];
 
 // Write your code below
+
+function avgPrice() {
+    let totalPrice = 0;
+
+    for (let price of amdPrices) { 
+        totalPrice += amdPrices[price];
+    }
+
+    // divide by length
+    let avg = 0;
+    avg = totalPrice / amdPrices.length;
+
+    console.log(avg);
+}
+
+avgPrice()
+
